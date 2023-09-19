ce7be504ae9baa7a69a416fd1f513e9c4f336a6a
David Richards <richards.malton@gmail.com>
2023-09-19 13:59:16 +0100

    Hidden obsolete software behind backup folder

--- software/DAIG_HSI_exerciser/Compass.h
+++
@@ -1,7 +0,0 @@
-#include <math.h>             // initial build using floating point, intent to rewrite with fixed point
-#include <SPI.h>
-#include <TFT_eSPI.h>         // Hardware-specific library
-
-
-extern void compass_init(void);
-extern void draw_needle(float, float);
--- software/DAIG_i2c_slave/json_server.h
+++
@@ -1,2 +0,0 @@
-extern void json_server_setup(void);
-extern void json_server_loop(void);
